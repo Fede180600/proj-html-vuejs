ec7a939610f2560c3ebe6cb6c1ce25e665b0b06f
Fede180600 <fede180600@libero.it>
2022-05-30 15:17:29 +0200

    header sizing

--- src/assets/components/AppHeader.vue
+++ src/assets/components/AppHeader.vue
@@ -1,13 +1,24 @@
 <template>
     <header>
-        
+        <div class="container">
+            <!-- HEADER CONTENT HERE -->
+        </div>
     </header>
 </template>
+
 <script>
 export default {
     name: 'AppHeader',
 }
 </script>
+
 <style lang="scss" scoped>
+@import "../style/variable.scss";
+
+header {
+    height: 100px;
     
+    // TEST
+    background-color: $brand-primary-color;
+}
 </style>
