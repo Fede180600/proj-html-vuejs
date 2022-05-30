7b88a07bc25be5be2d197c28b1ac65545f04981f
Fede180600 <fede180600@libero.it>
2022-05-30 14:35:45 +0200

    create basic structure and import my components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-    
+    <!-- HEADER -->
+    <AppHeader />
+    <!-- /HEADER -->
+
+    <!-- MAIN -->
+    <main></main>
+    <!-- MAIN -->
+
+    <!-- FOOTER -->
+    <AppFooter />
+    <!-- /FOOTER -->
+
   </div>
 </template>
 
 <script>
-
+import AppHeader from "./assets/components/AppHeader.vue";
+import AppFooter from "./assets/components/AppFooter.vue";
 export default {
   name: 'App',
   components: {
-    
+    AppHeader,
+    AppFooter
   }
 }
 </script>
