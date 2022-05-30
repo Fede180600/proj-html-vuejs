6b5843758b4965a75e9a226e145fa8528a5e1d44
Fede180600 <fede180600@libero.it>
2022-05-30 17:28:40 +0200

    AppCoursesSection & AppCourseCard basic layout

---
+++ src/assets/components/AppCourseCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="my_card">
+        <div class="img_container">
+            <img src="../img/photo-1496307042754-b4aa456c4a2d-370x200.jpeg" alt="">
+        </div>
+
+        <div class="text">
+            <h5>How to be a DJ? Make Elettronic Music</h5>
+            <small>Electronic</small>
+        </div>
+
+        <div class="container">
+            <ul class="row row-cols-3  card_list">
+                <li><i class="fas fa-signal"></i></li>
+                <li><i class="fas fa-list"></i></li>
+                <li><i class="fas fa-clock"></i></li>
+            </ul>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AppCourseCard',
+}
+</script>
+
+<style lang="scss" scoped>
+
+.my_card {
+    text-align: center;
+    padding: 0;
+    background-color: white;
+    width: calc(100% / 3 - 20px);
+    margin: 0 5px 20px 5px;
+    .card_list {
+        border-top: 1px solid lightgray;
+        padding: 10px 0 0 0;
+        font-size: 12px;
+        list-style: none;
+    }
+}
+</style>
---
+++ src/assets/components/AppCoursesSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <section style="background-color: #e4e7e7; padding: 50px 0; ">
+        <div class="container">
+            <h2 style="text-align: center; margin-bottom: 30px;">New Courses</h2>
+            <div class="row row-cols-3">
+                <AppCourseCard />
+                <AppCourseCard />
+                <AppCourseCard />
+                <AppCourseCard />
+                <AppCourseCard />
+                <AppCourseCard />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import AppCourseCard from "./AppCourseCard.vue";
+export default {
+    name: 'AppCoursesSection',
+    components: {
+        AppCourseCard
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
