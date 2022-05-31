5af16ee308b0c8ddb417667d27589edaafe9034d
Fede180600 <fede180600@libero.it>
2022-05-31 10:58:21 +0200

    AppWhySection basic layout

---
+++ src/assets/components/AppWhyCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="why_card">
+        <h3>Entrepreneurship</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci fugit iure recusandae ex natus excepturi consequatur, aperiam ipsam molestias eius praesentium nostrum corporis similique. Nobis nesciunt nemo molestias harum ipsum.</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AppWhyCard',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variable.scss";
+.why_card {
+    width: calc(100% / 2 - 20px);
+    margin-bottom: 30px;
+    border: 1px solid black;
+    -webkit-box-shadow: 0px 0px 13px 4px #85858596;
+    -moz-box-shadow: 0px 0px 13px 4px #85858596;
+    -o-box-shadow: 0px 0px 13px 4px #85858596;
+    box-shadow: 0px 0px 13px 4px #85858596;
+
+    // TEST
+    padding: 20px 60px;
+}
+</style>
---
+++ src/assets/components/AppWhySection.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="why_section">
+        <div class="container">
+            <h2 style="text-align: center; margin: 40px 0;">Why my Courses?</h2>
+            <div class="row row-cols-2 justify-content-between">
+                <AppWhyCard />
+                <AppWhyCard />
+                <AppWhyCard />
+                <AppWhyCard />
+            </div>
+        </div> 
+    </section>
+</template>
+
+<script>
+import AppWhyCard from "./AppWhyCard.vue";
+
+export default {
+    name: 'AppWhtSection',
+    components: {
+        AppWhyCard
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
