83aae95763c66042d9efacfebf771a7fc5ec26d2
Fede180600 <fede180600@libero.it>
2022-05-31 10:58:46 +0200

    AppStatisticsSection basic layout

---
+++ src/assets/components/AppStatisticsJumbotron.vue
@@ -0,0 +1,28 @@
+<template>
+<section class="statistics_jumbotron">
+    <div class="container">
+        <ul class="row row-cols-4">
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+        </ul>
+    </div>
+</section>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.statistics_jumbotron {
+    background-image: url(../img/counter-1919x1101.jpg);
+    background-position: top;
+    background-size: cover;
+    padding: 210px 0;
+}
+</style>
