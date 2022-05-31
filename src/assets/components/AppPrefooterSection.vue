c170b81f88d34c7f0cbe2f6382f7ad640341acc5
Fede180600 <fede180600@libero.it>
2022-05-31 10:59:09 +0200

    AppPrefooterSection basic layout

---
+++ src/assets/components/AppPrefooterSection.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="prefooter_section">
+        <div class="container">
+            <div class="row align-items-center flex-column">
+                <h3 style="">What Students Say</h3>
+                <div class="students_small_slider">
+
+                </div>
+                <p style="width: 50%;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, fuga hic? Odit error laboriosam et fugiat ex possimus consectetur eveniet dolorem, odio unde itaque, distinctio culpa. Debitis commodi odio beatae! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus sint iure, est at a quis vel quisquam asperiores illo tempora repellat sapiente eaque consectetur enim assumenda facilis, molestias praesentium recusandae.</p>
+            </div>
+            
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+.prefooter_section {
+    padding: 80px 0;
+    text-align: center;
+    .students_small_slider {
+        height: 60px;
+        width: 30%;
+        min-width: 350px;
+        margin: 20px 0;
+
+        // TEST
+        background-color: #292525;
+    }
+}
+</style>
