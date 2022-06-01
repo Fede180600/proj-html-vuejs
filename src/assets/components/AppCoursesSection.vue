6a4e186a6db16f6791f3f552bfc10d68378134c2
Fede180600 <fede180600@libero.it>
2022-06-01 17:06:39 +0200

    add button in AppCoursesSection

--- src/assets/components/AppCoursesSection.vue
+++ src/assets/components/AppCoursesSection.vue
@@ -1,10 +1,11 @@
 <template>
     <section style="background-color: #edf2f5; padding: 60px 0; ">
-        <div class="container">
+        <div class="container text-center">
             <h2 style="text-align: center; margin-bottom: 50px; font-size: 2.8rem; font-weight: 800;">New Courses</h2>
             <div class="row row-cols-3 justify-content-between">
                 <AppCourseCard v-for="(card, index) in coursesArray" :key="index" :courseCard="card"/>
             </div>
+            <button>Load More</button>
         </div>
     </section>
 </template>
@@ -23,4 +24,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../style/variable.scss";
+button {
+    font-family: 'roboto' sans-serif;
+    text-transform: uppercase;
+    color: white;
+    font-size: .8rem;
+    font-weight: 600;
+    background-color: $brand-primary-color;
+    border: none;
+    border-radius: 50px;
+    padding: 10px 20px;
+}
 </style>
