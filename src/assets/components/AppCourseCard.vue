f0dad63ed36c1b72b10a06fc76a1829c898f1951
Fede180600 <fede180600@libero.it>
2022-06-01 16:59:57 +0200

    add style at AppCoursesSection

--- src/assets/components/AppCourseCard.vue
+++ src/assets/components/AppCourseCard.vue
@@ -2,11 +2,12 @@
     <div class="my_card">
         <div class="img_container">
             <img :src="require(`../img/${courseCard.poster}`)" alt="">
+            <span v-if="courseCard.special">special</span>
         </div>
 
         <div class="text">
-            <h5>{{ courseCard.title }}</h5>
-            <small>{{ courseCard.type }}}</small>
+            <h4>{{ courseCard.title }}</h4>
+            <small>{{ courseCard.type }}</small>
         </div>
 
         <div class="container">
@@ -31,18 +32,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import "../style/variable.scss";
 .my_card {
     text-align: center;
     padding: 0;
     background-color: white;
     width: calc(100% / 3 - 20px);
     margin-bottom: 30px;
+    .img_container {
+        margin-bottom: 30px;
+        position: relative;
+        span {
+            background-color: $brand-secondary-color;
+            text-transform: uppercase;
+            color: white;
+            font-size: .6rem;
+            font-weight: 800;
+            border-radius: 5px;
+            padding: 2px 5px;
+            position: absolute;
+            top: 10px; 
+            right: 10px;
+        }
+    }
+    .text {
+        margin-bottom: 20px;
+        h4 {
+        font-size: 1.5rem;
+        font-weight: 800;
+        }
+        small {
+            font-family: 'Roboto', sans-serif;
+            color: $app-light-gray;
+        }
+    }
     .card_list {
         border-top: 1px solid lightgray;
         padding: 10px 0 0 0;
         font-size: 12px;
         list-style: none;
+        font-family: 'Roboto', sans-serif;
+        color: $app-dark-gray;
+        i {
+            color: $brand-primary-color;
+            font-size: 1rem;
+            margin-right: 5px;
+        }
     }
 }
 </style>
