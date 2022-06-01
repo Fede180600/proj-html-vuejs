9c53ef75394ebd4d017af5517c9677151f85e9dd
Fede180600 <fede180600@libero.it>
2022-06-01 15:04:03 +0200

    add dynamic whyCard and style

--- src/assets/components/AppWhyCard.vue
+++ src/assets/components/AppWhyCard.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="why_card">
-        <h3>Entrepreneurship</h3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci fugit iure recusandae ex natus excepturi consequatur, aperiam ipsam molestias eius praesentium nostrum corporis similique. Nobis nesciunt nemo molestias harum ipsum.</p>
+        <i :class="whyCard.icon"></i> 
+        <h3>{{ whyCard.title }}</h3>
+        <p>{{ whyCard.text }}</p>
     </div>
 </template>
 
@@ -9,21 +10,38 @@
 
 export default {
     name: 'AppWhyCard',
+    props: {
+        whyCard: Object
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "../style/variable.scss";
 .why_card {
+    position: relative;
     width: calc(100% / 2 - 20px);
     margin-bottom: 30px;
-    border: 1px solid black;
+    padding: 40px 80px;
+    border-left: 6px solid $brand-primary-color;
     -webkit-box-shadow: 0px 0px 13px 4px #85858596;
     -moz-box-shadow: 0px 0px 13px 4px #85858596;
     -o-box-shadow: 0px 0px 13px 4px #85858596;
     box-shadow: 0px 0px 13px 4px #85858596;
-
-    // TEST
-    padding: 20px 60px;
+    i {
+        position: absolute; 
+        top: 40px; left: 20px; 
+        font-size: 2.5rem; 
+        color: $brand-primary-color;
+    }
+    h3 {
+        font-weight: 800;
+        margin-bottom: 15px;
+    }
+    p {
+        color: $app-light-gray;
+        font-size: .9rem;
+        line-height: 25px;
+    }
 }
 </style>
--- src/assets/components/AppWhySection.vue
+++ src/assets/components/AppWhySection.vue
@@ -1,12 +1,9 @@
 <template>
     <section class="why_section">
         <div class="container">
-            <h2 style="text-align: center; margin: 40px 0;">Why my Courses?</h2>
+            <h2 style="text-align: center; margin-bottom: 50px; font-weight: 800; font-size: 2.8rem;">Why my Courses?</h2>
             <div class="row row-cols-2 justify-content-between">
-                <AppWhyCard />
-                <AppWhyCard />
-                <AppWhyCard />
-                <AppWhyCard />
+                <AppWhyCard v-for="(why, index) in whyArray" :key="index" :whyCard="why"/>
             </div>
         </div> 
     </section>
@@ -16,12 +13,18 @@
 import AppWhyCard from "./AppWhyCard.vue";
 
 export default {
-    name: 'AppWhtSection',
+    name: 'AppWhySection',
     components: {
         AppWhyCard
+    },
+    props: {
+        whyArray: Array
     }
 }
 </script>
 
 <style lang="scss" scoped>
+.why_section {
+    padding: 60px 0;
+}
 </style>
