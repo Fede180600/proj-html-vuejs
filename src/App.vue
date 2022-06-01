c584f3993f91615b7f03ebc17d72f37e821eb41b
Fede180600 <fede180600@libero.it>
2022-06-01 12:54:19 +0200

    props to send data at AppCoursesSection

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <!-- /banner top -->
 
       <!-- courses section -->
-      <AppCoursesSection />
+      <AppCoursesSection :coursesArray="courses"/>
       <!-- /courses section -->
 
       <!-- my book jumbotron -->
@@ -109,6 +109,62 @@ export default {
           number: 34,
           description: "Seconds"
         },
+      ],
+      courses: [
+        {
+          poster: "photo-1496307042754-b4aa456c4a2d-370x200.jpeg",
+          title: "How to be a DJ? Make Elettronic Music",
+          type: "Electronic",
+          level: "Advanced",
+          lectures: "8 Lectures",
+          duration: "6 Hours",
+          special: true
+        },
+        {
+          poster: "12345-1-370x200.png",
+          title: "Nvidia and UE4 Technologies Practice",
+          type: "Nvidia",
+          level: "Advanced",
+          lectures: "8 Lectures",
+          duration: "6 Hours",
+          special: true
+        },
+        {
+          poster: "photo-1491897554428-130a60dd4757-370x200.jpeg",
+          title: "Fashion Photography from professional",
+          type: "Fashion",
+          level: "Advanced",
+          lectures: "6 Lectures",
+          duration: "6 Hours",
+          special: true
+        },
+        {
+          poster: "photo-1416339134316-0e91dc9ded92-370x200.jpeg",
+          title: "Design Instruments for Communication",
+          type: "Comunication",
+          level: "Intermediate",
+          lectures: "6 Lectures",
+          duration: "6 Hours",
+          special: null
+        },
+        {
+          poster: "cathryn-lavery-67852-unsplash-370x200.jpg",
+          title: "Make your Concept Right and Beautiful",
+          type: "Art",
+          level: "Intermediate",
+          lectures: "6 Lectures",
+          duration: "6 Hours",
+          special: null
+        },
+        {
+          poster: "photo-1475452779376-caebfb988090-370x200.jpeg",
+          title: "Road Bike Manual or How to Be a Champion.",
+          type: "Bicycling",
+          level: "Advanced",
+          lectures: "6 Lectures",
+          duration: "6 Hours",
+          special: null
+        }
       ]
     }
   },
