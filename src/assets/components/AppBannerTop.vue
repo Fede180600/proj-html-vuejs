aaef3e0371e981c7a938cd5ac900e08134e88789
Fede180600 <fede180600@libero.it>
2022-06-01 10:38:47 +0200

    AppBannerTop add title

--- src/assets/components/AppBannerTop.vue
+++ src/assets/components/AppBannerTop.vue
@@ -1,18 +1,39 @@
 <template>
     <div class="banner">
-        <div class="banner_top"></div>
-    
+        <div class="banner_top">
+            <div class="container">
+                <div class="banner_text">
+                    <h2>
+                        Start Your Human
+                        <br>
+                        <span class="text_brand_primary_color">Revolution</span>
+                        Today
+                    </h2>
+                    <p>A wonderful serenity taken possession of my entire soul, like these sweet mornings of spring which i enjoy with my whole heart</p>
+                </div>
+
+            </div>
+        </div>
+
         <div class="banner_middle"></div>
 
         <div class="banner_bottom">
             <div class="container">
                 <div class="row row-cols-2">
-                    <div style="border: 1px solid black; width: 45%; display: flex; flex-direction: column; justify-content: center;">
+                    <div
+                        style="border: 1px solid black; width: 45%; display: flex; flex-direction: column; justify-content: center;">
                         <h2>Hi there, my name is Jhon Doe, I'm the founder of the Brain Accademy</h2>
                         <img src="../img/sign.jpg" alt="firma Jhon Doe">
                     </div>
                     <div>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis voluptatibus ullam a ducimus quidem, molestiae aut officiis aliquam eum tenetur facilis aliquid nihil deleniti et quam quibusdam praesentium commodi sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sequi sed aut labore sint tempora accusamus cumque laborum beatae tenetur, nisi aspernatur sit odio ipsum, odit, eos nulla praesentium ullam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores porro omnis corrupti minima nobis cum ratione laboriosam officiis velit laudantium perferendis repellat veniam temporibus ullam quidem aliquam, culpa, provident expedita!</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis voluptatibus ullam a
+                            ducimus quidem, molestiae aut officiis aliquam eum tenetur facilis aliquid nihil deleniti et
+                            quam quibusdam praesentium commodi sed. Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Expedita sequi sed aut labore sint tempora accusamus cumque laborum beatae tenetur,
+                            nisi aspernatur sit odio ipsum, odit, eos nulla praesentium ullam! Lorem ipsum dolor sit
+                            amet consectetur adipisicing elit. Maiores porro omnis corrupti minima nobis cum ratione
+                            laboriosam officiis velit laudantium perferendis repellat veniam temporibus ullam quidem
+                            aliquam, culpa, provident expedita!</p>
                     </div>
                 </div>
             </div>
@@ -32,22 +53,42 @@ export default {
 .banner {
     position: relative;
 
-    // TEST
-    background-color: orangered;
     &_top {
-        height: 600px;
-        border: 1px solid white;
+        background-image: url(../img/slide-1.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        // height: 900px;
+        padding: 400px 0;
+        margin-top: 100px;
+
+        .banner_text {
+            width: 24%;
+            position: absolute;
+            top: 22%;
+            right: 15vw;
+            transform: translate(-50%, -50%);
+            h2 {
+                font-weight: 800;
+                font-size: 3rem;
+            }
+            p {
+                font-size: 18px;
+                color: #363636;
+            }
+        }
     }
+
     &_middle {
         height: 200px;
-        width: 90%;
+        width: 65%;
         background-color: $brand-primary-color;
         position: absolute;
         left: 50%;
-        top: 30%;
-        transform: translate(-50% , 50%);
+        top: 40%;
+        transform: translate(-50%, 50%);
         border-radius: 10px;
     }
+
     &_bottom {
         padding: 200px 0 50px 0;
     }
