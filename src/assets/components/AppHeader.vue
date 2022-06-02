7dd96cbb9a8f4fafc749e2d2c966bc0de5f44e61
Fede180600 <fede180600@libero.it>
2022-06-02 15:29:48 +0200

    add border-bottom at heade li in hover

--- src/assets/components/AppHeader.vue
+++ src/assets/components/AppHeader.vue
@@ -8,10 +8,11 @@
                 <ul class="d-flex justify-content-between align-items-center me-5 flex-grow-1">
                     <li v-for="item in headerNavItems" :key="item.id" style="position: relative;">
                         {{ item.content }}
-                        <span v-if="item.new" style="position: absolute; top: -10px; right: -5px; background-color: #dc1245; color: white; border-radius: 5px; padding: 0 5px; font-size: 10px; text-transform: uppercase;">new</span>
+                        <span v-if="item.new"
+                            style="position: absolute; top: -10px; right: -5px; background-color: #dc1245; color: white; border-radius: 5px; padding: 0 5px; font-size: 10px; text-transform: uppercase;">new</span>
                         <span v-else></span>
                     </li>
-                    
+
                 </ul>
                 <div class="socials-container d-flex align-items-center" style="font-size: 600;">
                     <i class="fab fa-linkedin"></i>
@@ -44,24 +45,33 @@ header {
     left: 0;
     z-index: 1000;
     width: 100%;
+
     .my_container {
         height: 100%;
+
         nav {
-        width: 50%;
-        ul {
-            padding: 0;
-            margin: 0;
-            list-style: none;
-            height: 40px;
-            font-weight: 600;
-            li {
-                cursor: pointer;
+            width: 50%;
+
+            ul {
+                padding: 0;
+                margin: 0;
+                list-style: none;
+                height: 40px;
+                font-weight: 600;
+
+                li {
+                    cursor: pointer;
+                    padding-bottom: 10px;
+                }
+            }
+
+            i {
+                margin: 0 5px;
             }
         }
-        i {
-            margin: 0 5px;
-        }
     }
-    }
+}
+li:hover {
+    border-bottom: 4px solid $brand-primary-color;
 }
 </style>
