2c9df2c31b710f781d3248085bb4e21df475a238
Fede180600 <fede180600@libero.it>
2022-06-02 15:29:27 +0200

    add hover effect at AppWhyCard

--- src/assets/components/AppWhyCard.vue
+++ src/assets/components/AppWhyCard.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="why_card">
+        <div class="yellow_border"></div>
         <i :class="whyCard.icon"></i> 
         <h3>{{ whyCard.title }}</h3>
         <p>{{ whyCard.text }}</p>
@@ -20,14 +21,23 @@ export default {
 @import "../style/variable.scss";
 .why_card {
     position: relative;
-    width: calc(100% / 2 - 20px);
+    width: calc(100% / 2 - 10px);
+    min-height: 300px;
+    border: 15px solid none;
     margin-bottom: 30px;
     padding: 40px 80px;
-    border-left: 6px solid $brand-primary-color;
     -webkit-box-shadow: 0px 0px 13px 4px #85858596;
     -moz-box-shadow: 0px 0px 13px 4px #85858596;
     -o-box-shadow: 0px 0px 13px 4px #85858596;
     box-shadow: 0px 0px 13px 4px #85858596;
+    .yellow_border {
+        width: 6px;
+        height: 100%;
+        background-color: $brand-primary-color;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
     i {
         position: absolute; 
         top: 40px; left: 20px; 
@@ -39,9 +49,20 @@ export default {
         margin-bottom: 15px;
     }
     p {
+        font-family: 'Roboto', sans-serif;
         color: $app-light-gray;
         font-size: .9rem;
         line-height: 25px;
+    }
+}
+.why_card:hover {
+    background-color: $brand-primary-color;
+    border: 15px solid white;
+    p {
+        color: black;
+    }
+    i {
+        color: white;
     }
 }
 </style>
